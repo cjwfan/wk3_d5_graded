b06d955f0070a58f673872270a79269c5a8d9174
Mia C <mcollette101@gmail.com>
2026-02-22 16:49:25 -0600

    aded for Loop

--- app.js
+++ app.js
@@ -2,3 +2,24 @@
 
 const tags = ["js", "week3"];
 const scores = [10, 25, 18, 30];
+console.log(scores.length);
+console.log(tags.length)
+let totalScore = 0;
+
+for (let i = 0; i <scores.length; i++) {
+    totalScore += scores[i];
+   console.log(totalScore);
+    
+}
+console.log(totalScore);
+
+tags.forEach(function(tag){
+    
+console.log(tag);
+ 
+ });
+
+ const doubledScores = scores.map(function (score) {
+   return score * 2;
+ });
+ console.log(doubledScores);
